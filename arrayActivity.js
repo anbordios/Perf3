71a4b59b7e643fcbf98696ba97731c7162e7e63b
anbordios <annbordios15@gmail.com>
2024-10-16 20:05:19 +0800

    arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,13 @@
+// Declare an array and enter favorite singers and store it.
+let favSingers = ["Bryan adams", "Bon Jovi", "Taylor Swift"];
+console.log(favSingers[0]);
+
+// Create another array to store the favorite numbers.
+let favNumbers = [7, 5, 3, 15];
+// Create an array to store different data types.
+let mixedArr = ["Music", ["Song"], 123, true];
+
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);

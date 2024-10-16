f49b53287e5b6af2c49b3ec631775800c7b3cf47
anbordios <annbordios15@gmail.com>
2024-10-16 21:39:30 +0800

    objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+  type: "SUV",
+  model: "Ford Explorer",
+  color: "Blue"
+};
+
+console.log(car.type); // "SUV"
+
+car.type = "Toyota"; // Update type
+console.log(car);
+
+car.wheels = 4; //Add new property 
+console.log(car);
